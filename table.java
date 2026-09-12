68670ec3c7c6545c3855208fc49ba8dc58066d83
keshavpoilk369-tech <keshavpoilk369@gmail.com>
2026-09-12 08:46:58 +0530

    Add files via upload

---
+++ table.java
@@ -0,0 +1,27 @@
+package Aimaster;
+import java.util.Scanner;
+public class table {
+    static void main() {
+      Scanner sc = new Scanner(System.in);
+        System.out.print("ENTER THE TABLE WHICH YOU WANT TO SEE : ");
+        int n = sc.nextInt();
+
+//        for(int i = 1; i < 11; i++){
+//            System.out.println(n*i);
+//        }
+//-------------------------------------------------------------------------------------------------
+//        int i = 1;
+//        while(i < 11){
+//            System.out.println(n*i);
+//            i++;
+//        }
+//----------------------------------------------------------------------------------------------------
+//        int i = 1;
+//        do{
+//            System.out.println(n*i);
+//            i++;
+//        }while(i < 11);
+
+
+    }
+}
